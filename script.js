ac02558ec1db006c69b1cefe5c998c0a8a26a4a1
Jeremy Savage <52190825+jeremyssavage@users.noreply.github.com>
2019-12-31 15:36:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
 request.onload = function() {
-  // Begin accessing JSON data here
+  // Begin accessing JSON data here.
+  var data = JSON.parse(this.response)
+  
+  data.forEach(movie => {
+    // Log each movie's title.
+    console.log(movie.title)
 }
 
 // Send request.
