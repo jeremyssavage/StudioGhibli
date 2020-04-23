23401b19675370f2aba653c607dc5dc4e668d907
Jeremy Savage <52190825+jeremyssavage@users.noreply.github.com>
2020-04-24 01:24:34 +1000

    Update scripts.js
    Trying to add logo and container

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
+app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+app.appendChild(logo);
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+app.appendChild(container);
+
 fetch('https://ghibliapi.herokuapp.com/films')
   .then(response => {
-    return response.json()
+    return response.json();
   })
   .then(data => {
-    document.write('Data loaded successfully!')
-    console.log(data)
+    app = document.findElementById('root');
   })
   .catch(err => {
-    document.write('Some sort of error!')
-    console.log(err)
+    console.log(err);
   })
 
