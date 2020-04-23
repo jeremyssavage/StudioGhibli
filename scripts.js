16f166230c775ee045ffbcc2decd48d3acb25993
Jeremy Savage <52190825+jeremyssavage@users.noreply.github.com>
2020-04-24 01:43:24 +1000

    Update scripts.js
    Made the cards with titles and descriptions.

--- scripts.js
+++ scripts.js
@@ -14,6 +14,20 @@ fetch('https://ghibliapi.herokuapp.com/films')
   })
   .then(data => {
     app = document.findElementById('root');
+  
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      container.appendChild(card);
+      
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+      card.appendChild(h1);
+      
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+      card.append(p);
   })
   .catch(err => {
     console.log(err);
